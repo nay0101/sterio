b5974ec5a5a6dc94bcfce886e5c61b7f597e86d2
nay0101 <naylinaung.projects@gmail.com>
2023-05-31 15:58:19 +0800

    Quantity Bug Fix

--- client/src/pages/SingleProduct.jsx
+++ client/src/pages/SingleProduct.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { Add, Remove } from '@mui/icons-material';
-import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { Add, Remove } from "@mui/icons-material";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import { publicRequest } from '../request-methods';
-import { addProduct } from '../store/cart-slice';
+import { publicRequest } from "../request-methods";
+import { addProduct } from "../store/cart-slice";
 
-import Navbar from '../layout/Navbar';
-import Announcement from '../layout/Announcement';
-import Footer from '../layout/Footer';
-import Newsletter from '../components/Newsletter';
+import Navbar from "../layout/Navbar";
+import Announcement from "../layout/Announcement";
+import Footer from "../layout/Footer";
+import Newsletter from "../components/Newsletter";
 
 const SingleProduct = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const [product, setProduct] = useState({});
   let [quantity, setQuantity] = useState(1);
-  let [size, setSize] = useState('S');
+  let [size, setSize] = useState("S");
   const getProduct = async () => {
     try {
       const url = `/products/${id}`;
@@ -40,21 +40,21 @@ const SingleProduct = () => {
     <>
       <Announcement />
       <Navbar />
-      <section className='p-8 grid md:grid-cols-2 gap-8'>
-        <div className='grow'>
+      <section className="p-8 grid md:grid-cols-2 gap-8">
+        <div className="grow">
           <img
             src={product.image}
             alt={product.title}
-            className='w-full h-full object-cover'
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className='grow'>
-          <h2 className='text-5xl mb-6'>{product.title}</h2>
-          <p className='mb-6 text-xl'>{product.description}</p>
-          <span className='block mb-6 text-4xl'>$ {product.price}</span>
-          <div className='grid sm:grid-cols-2 gap-4 mb-6'>
+        <div className="grow">
+          <h2 className="text-5xl mb-6">{product.title}</h2>
+          <p className="mb-6 text-xl">{product.description}</p>
+          <span className="block mb-6 text-4xl">$ {product.price}</span>
+          <div className="grid sm:grid-cols-2 gap-4 mb-6">
             <div>
-              <label htmlFor='' className='text-xl'>
+              <label htmlFor="" className="text-xl">
                 Size
               </label>
               <select onChange={sizeChangeHandler}>
@@ -66,23 +66,23 @@ const SingleProduct = () => {
               </select>
             </div>
           </div>
-          <div className='grid sm:grid-cols-2 gap-4 mb-6'>
-            <div className='flex items-center justify-start'>
+          <div className="grid sm:grid-cols-2 gap-4 mb-6">
+            <div className="flex items-center justify-start">
               <span
-                className='cursor-pointer'
+                className="cursor-pointer"
                 onClick={() => {
-                  quantity > 1 && setQuantity(quantity--);
+                  quantity > 1 && setQuantity((prevState) => prevState - 1);
                 }}
               >
                 <Remove />
               </span>
-              <span className='mx-2 text-xl h-10 w-10 rounded-2xl border flex justify-center items-center'>
+              <span className="mx-2 text-xl h-10 w-10 rounded-2xl border flex justify-center items-center">
                 {quantity}
               </span>
               <span
-                className='cursor-pointer'
+                className="cursor-pointer"
                 onClick={() => {
-                  setQuantity(quantity++);
+                  setQuantity((prevState) => prevState + 1);
                 }}
               >
                 <Add />
@@ -91,7 +91,7 @@ const SingleProduct = () => {
             <div>
               <button
                 onClick={addToCartHandler}
-                className='uppercase hover:bg-teal-700 hover:text-white transition ease-out duration-500 border-teal-700 border rounded p-4'
+                className="uppercase hover:bg-teal-700 hover:text-white transition ease-out duration-500 border-teal-700 border rounded p-4"
               >
                 Add to cart
               </button>
