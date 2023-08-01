8fe70f7af5e08f3d34dd9089c29002ccf0ab327a
nay0101 <naylinaung.projects@gmail.com>
2023-08-01 18:23:03 +0800

    Remove Temp Folder

--- admin/src/pages/dashboard/index.js
+++
@@ -1,5 +0,0 @@
-export * from "@/pages/dashboard/home";
-export * from "@/pages/dashboard/profile";
-export * from "@/pages/dashboard/UsersTable";
-export * from "@/pages/dashboard/MoviesTable";
-export * from "@/pages/dashboard/notifications";
